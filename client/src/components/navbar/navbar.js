fdc6de61c155ae76b9408f62604d3abb03b0bb0a
Tez, Gardner <tezgardner1212@gmail.com>
2019-11-23 02:52:25 -0600

    added navbar

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,22 +1,22 @@
 import React from "react";
+import navbarCss from "../../css/main/navbar.css"
+class Navbar extends React.Component{
 
-class Navbar extends React.Component {
 
-    render() {
-        return (
-            <div>
-                <div style={{ "backgroundColor": "white" }}></div>
-                <nav className="navbar navbar-dark">
-                    <a className="navbar-brand" href="/">
-                    <h1 style={{"color":"red"}}>X-translate</h1>
-                    </a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                </nav>
-            </div>
-        )
-    }
+    render(){
+        return(
+        <div>
+        <nav className="navbar navbar-dark" style={navbarCss} id="navbar">
+            <a className="navbar-brand" href="/">
+                <img src="./translationLogo.png"  alt="TranlateThat Logo" style={{"marginTop":"-25px"}}/>
+            </a>
+           
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+        </nav>
+    </div>
+        )}
 }
 
 export default Navbar
